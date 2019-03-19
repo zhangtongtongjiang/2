14090175fd6938b6fafde79b215f1239697b5e17
zhangtongtongjiang <1278217762@qq.com>
2019-03-19 19:41:10 +0800

    4thinkphp修改博文/构造方法_initialize的使用

--- thinkphp/Application/Home/Controller/IndexController.class.php
+++ thinkphp/Application/Home/Controller/IndexController.class.php
@@ -3,94 +3,123 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function test()
-	{
-        $last_sql=get_sql();
-		var_dump($last_sql);
-/*		读
-		$website_url=C("website_url");
-		$this->assign("website_url",$website_url);
-		$kkkkk_model=M("persons");
-		$kkkkk_list=$kkkkk_model->where("id>1")->select();
-		var_dump($kkkkk_list);exit();
-			*/
-		
-		//增加
-/*		$data["id"]=4;
-		$data["FirstName"]=pp;
-		$data["Age"]=99;
-		$res=M('persons')->add($data);
-		var_dump($res);*/
-		
-		//改
-/*		1where实现
-		$data["FirstName"]=qqq;
-		$data["Age"]=66;
-		$res=M('persons')->where("id=4")->save($data);
-		var_dump($res);*/
-		/*2save主键实现
-		$data["id"]=4;	
-		$data["FirstName"]=yy2;
-		$data["Age"]=67;
-		$res=M('persons')->save($data);
-		var_dump($res);	*/
-		
-		//删
-/*		1
-		$map['id']=3;
-		$res=M('persons')->where($map)->delete();
-        //echo M()->getLastSql();显示
-		var_dump($res);	*/	
-		//2
-/*		$map['id']=array("lt",2);
-		$res=M('persons')->where($map)->delete();
-        //echo M()->getLastSql();
-		var_dump($res);*/
-		
-		$this->assign("array",array("id"=>"1","name"=>"aa"));
-		$this->assign("array2",range(1,10));
-		$this->display();
-	}
+    {
 
+        //读
+         $blog_model=D("Blog");
+        // $list=$blog_model->get_all_blog();
+        // var_dump($list);
+        // exit;
+        // $website_url=C("website_url");
+        // $blog_model=M("blog");
 
-    public function index()
-	{
-		//取出所有博文数据
-		//模型模型取出数据
-        $blog_list=D('blog')->get_all_blog();
-		$this->assign("blog_list",$blog_list);//所有博文
-		$this->display();//加载模板
-		
-    } 
-	
-	public function add_blog()
-	{
+        // $blog_list=$blog_model->field("id,title")->where("id>0")->order(" id desc")->limit(1)->select();
+        // var_dump($blog_list);exit();
 
-		$this->display();//加载模板
-		
+        //增
+        // $data['id']=3;
+        // $data['title']="标题3";
+        // $data['content']="内容3";
+        // $res=M('blog')->add($data);//增加  add方法返回刚刚插入的id
+        // var_dump($res);
+        //改
+            //1.where实现
+            // $data['title']="标题3333";
+            // $data['content']="内容3333";
+            // $res=M('blog')->where("id=3")->save($data);//修改  save方法返回真假
+            // var_dump($res);
+            // 2.save主键实现
+            // $data['id']=3;
+            // $data['title']="标题3333";
+            // $data['content']="内容3333";
+            // $res=M('blog')->save($data);//修改  save方法返回真假
+            // var_dump($res);
+        //删
+        // $map['id']=array("lt",2);
+        // $res=M('blog')->where($map)->delete();//增加  add方法返回刚刚插入的id
+        // // echo M()->getLastSql();
+        // var_dump($res);
+        $this->assign("website_url",$website_url);
+        $this->assign("key","value");
+        $this->assign("array",array("id"=>"1","name"=>"lizhaoyao"));
+        $this->assign("array2",range(1,10));//创建1到10的数组
+        $this->display();
     }
-	
-	
 
+    /*
+     * 使用TP封装的构造方法
+     * **/
+    function _initialize()
+    {
+        $this->blog_model=D('Blog');
+    }
+    /*
+     * 博文列表页面
+     * 取出所有博文 列表形式在展现出来
+     * **/
+    public function index()
+    {
+        //取出所有博文数据
+        //通过模型取出数据
+        $blog_list=$this->blog_model->get_all_blog(10);
+        $this->assign('blog_list',$blog_list);//所有博文
+        $this->display();//加载模板
+    }
 
-	
-	public function save_blog()
+    /*
+     * 增加博文展示页面
+     * **/
+    public function add_blog()
+    {
+        $this->display();//加载模板
+    }
+    /*
+     * 编辑博文展示页面
+     * **/
+    public function edit_blog()
+    {
+        $id=I('get.id');//接收博文id
+        $blog_info=$this->blog_model->find($id);//查询博文信息
+        $this->assign('blog_info',$blog_info);
+        $this->display();//加载模板
+    }
+    /*
+     * 博文保存和添加修改
+     * **/
+    public function save_blog()
     {
         $title=I('post.title');//接收标题
         $content=I('post.content');//接收内容
+        $id=I('post.id')+0;//接收id并且取整
         $data=compact("title","content");
-        $data['add_time']=time();//添加时间
-        $res=D("Blog")->add($data);
-		if($res)
+        if($id>0)
         {
-            $this->success("添加博文成功",U('/Home/Index/index'));
+            //编辑
+            $map['id']=$id;
+            $res=$this->blog_model->where($map)->save($data);
+            if($res)
+            {
+                $this->success("修改博文成功",U('/Home/Index/index'));
+            }
+            else{
+                $this->error("修改博文失败",U('/Home/Index/index'));
+            }
+        }else
+        {
+            //增加
+            $data['add_time']=time();//添加时间
+            $res=$this->blog_model->add($data);
+            if($res)
+            {
+                $this->success("添加博文成功",U('/Home/Index/index'));
+            }
+            else{
+                $this->error("添加博文失败",U('/Home/Index/index'));
+            }
         }
-        else{
-            $this->error("添加博文失败",U('/Home/Index/index'));}
-	
+
     }
-	
-	
-	    /*
+    /*
      * 删除博文
      *
      * **/
@@ -98,7 +127,7 @@ class IndexController extends Controller {
     {
         $id=I('get.id');
         $map['id']=$id;
-        $res=D('Blog')->where($map)->delete();
+        $res=$this->blog_model->where($map)->delete();
         if($res)
         {
             $this->success("删除博文成功",U('/Home/Index/index'));
@@ -107,7 +136,4 @@ class IndexController extends Controller {
             $this->error("删除博文失败",U('/Home/Index/index'));
         }
     }
-	
 }
-	
-	
---
+++ thinkphp/Application/Runtime/Cache/Home/e592934bb3d22505f3288cc7d81de320.php
@@ -0,0 +1,64 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>编辑博文</title>
+    <style>
+        *{
+            margin: 0px;
+            padding: 0px;
+        }
+        body{
+            background: #ccc;
+        }
+        #blog_list{
+            margin: 40px auto;
+            width: 70%;
+            border: solid 10px #ddd;
+            padding: 20px;
+            background: #CEE5D0;
+        }
+        #blog_list table{width: 100%;}
+        #blog_list table th{text-align: left;}
+        #head{
+            margin: 40px auto;
+            width: 70%;
+            border: solid 10px #ddd;
+            padding: 20px;
+        }
+        #head a {
+            text-decoration: blink;
+        }
+    </style>
+</head>
+<body>
+    <div id="head">
+        <a href="<?php echo U('/Home/Index/add_blog');?>">增加博文</a>
+        <a href="<?php echo U('/Home/Index/index');?>">博文列表</a>
+    </div>
+    <div id="blog_list">
+        <form action="<?php echo U('/Home/Index/save_blog');?>" method="post">
+            <table>
+                <tr>
+                    <td width="100px;">博文标题</td>
+                    <td><input type="text" name="title" value="<?php echo ($blog_info["title"]); ?>"/></td>
+                </tr>
+                <tr>
+                    <td width="100px;">博文内容</td>
+                    <td>
+                        <textarea name="content" id="" cols="80" rows="10"><?php echo ($blog_info["content"]); ?></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type="hidden" name="id" value="<?php echo ($blog_info["id"]); ?>">
+                        <input type="submit" value="修改">
+                        <input type="reset" value="重置">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</body>
+</html>
